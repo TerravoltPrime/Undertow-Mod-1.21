b3604046df6255f5376fad2e3a042528c363a1fd
terra <terravolt.mc@gmail.com>
2024-10-12 20:57:41 -0400

    Umber implementation Take 2

---
+++ src/main/java/net/terra/undertow/item/ModItems.java
@@ -0,0 +1,18 @@
+package net.terra.undertow.item;
+
+import net.terra.undertow.UnderTow;
+import net.minecraft.world.item.Item;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public class ModItems {
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(UnderTow.MOD_ID);
+
+
+
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
